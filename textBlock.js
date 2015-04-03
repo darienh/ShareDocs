3be6ebd19b7b0a03b2e0ccddb4fd38a37a78b945
Darien <huan8898@vandals.uidaho.edu>
2015-04-03 11:54:54 -0700

    Create textBlock.js

---
+++ textBlock.js
@@ -0,0 +1,10 @@
+//
+//for a block of text
+//
+var label2 = Ti.UI.createLabel({
+  color:'blue',
+  text: 'A long label with\na few line breaks\nand unicode (UTF8)\nsymbols such as\na white chess piece \u2655\nand the euro symbol \u20ac\nlooks like this!\n',
+  textAlign: Ti.UI.TEXT_ALIGNMENT_LEFT,
+  top: 30,
+  width: 300, height: 200
+});
